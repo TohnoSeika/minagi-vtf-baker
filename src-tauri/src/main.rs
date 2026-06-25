bee5378e570b52df281c074141023f6aa2a1aed5
Tohno Seika <tohnoseika@users.noreply.github.com>
2026-06-26 03:22:07 +0800

    🗑️ 移除代码文件，仓库仅保留项目介绍

--- src-tauri/src/main.rs
+++
@@ -1,7 +0,0 @@
-// 这个文件是桃华帮 Minagi 写的哦 ♡
-// Prevents a console window from appearing on Windows in release builds
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    minagi_vtf_baker::run()
-}
